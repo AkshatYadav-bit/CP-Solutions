fa5684efd4e5e9d5a08f65c6b01d364d58b05cfb
Akshat Yadav <akshatyadavcv@gmail.com>
2026-09-01 12:09:47 +0530

    Add Domino piling (800)

---
+++ codeforces/800/50A - Domino piling.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class FastScanner {
+        private final BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+        char nextChar() throws IOException {
+            return next().charAt(0);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        FastScanner fs = new FastScanner();
+        StringBuilder out = new StringBuilder();
+        int m = fs.nextInt();
+        int n = fs.nextInt();
+        out.append((m*n)/2);
+        System.out.println(out);
+
+        //System.out.print(out);
+    }
+}
