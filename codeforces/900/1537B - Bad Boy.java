6f6bf800ed7635d2524f490f6de4dcaec72bcea8
Akshat Yadav <akshatyadavcv@gmail.com>
2026-09-02 10:56:21 +0530

    Add Bad Boy (900)

---
+++ codeforces/900/1537B - Bad Boy.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class FastScanner {
+        private final BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+        char nextChar() throws IOException {
+            return next().charAt(0);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        FastScanner fs = new FastScanner();
+        StringBuilder out = new StringBuilder();
+
+        int t = fs.nextInt();
+
+        while (t-- > 0) {
+            int n = fs.nextInt();
+            int m = fs.nextInt();
+            int i = fs.nextInt();
+            int j = fs.nextInt();
+            out.append(1+" "+1+" "+n+" "+m).append('\n');
+            
+
+            
+        }
+
+        System.out.print(out);
+    }
+}
