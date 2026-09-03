d55ad848e26d2c46ca14d2a53b4c421e20af90a3
Akshat Yadav <akshatyadavcv@gmail.com>
2026-09-03 14:18:55 +0530

    Add Construct-Uniform-Parity-Array-II (array)

--- leetcode/array/3876-Construct-Uniform-Parity-Array-II.java
+++ leetcode/array/3876-Construct-Uniform-Parity-Array-II.java
@@ -1,22 +1,14 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
         int minOdd = Integer.MAX_VALUE;
-        int secminOdd = Integer.MAX_VALUE;
         int minEven = Integer.MAX_VALUE;
         for(int x : nums1){
             if(x % 2 == 0){
                 minEven = Math.min(minEven,x);
             }else{
-                if(x < minOdd){
-                    minOdd = x;
-                }else if(x < secminOdd){
-                    secminOdd = x;
-                }
+                minOdd = Math.min(minOdd,x);
             }
         }
-        // if((secminOdd - minOdd >= 1 && secminOdd != Integer.MAX_VALUE)|| minEven == Integer.MAX_VALUE){
-        //     return true;
-        // }
         if(minEven - minOdd >=1 || minOdd == Integer.MAX_VALUE) {
             return true;
         }
