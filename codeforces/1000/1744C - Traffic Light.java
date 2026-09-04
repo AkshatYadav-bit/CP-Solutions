2d89d4423097a975ed656921c9e88cea5cf8286a
Akshat Yadav <akshatyadavcv@gmail.com>
2026-09-04 19:28:33 +0530

    Add Traffic Light (1000)

---
+++ codeforces/1000/1744C - Traffic Light.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class FastScanner {
+        private final BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+        char nextChar() throws IOException {
+            return next().charAt(0);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        FastScanner fs = new FastScanner();
+        StringBuilder out = new StringBuilder();
+
+        int t = fs.nextInt();
+
+        while (t-- > 0) {
+            int n = fs.nextInt();
+            char c = fs.nextChar();
+            String s = fs.next();
+            s = s + s;
+            char[] arr = s.toCharArray();
+            int ans = 0;
+            int i = 0;
+            int j = 0;
+            while( j < n){
+                if(arr[j] == c && c != 'g'){
+                    i = j;
+                    while (arr[j] != 'g' && j < 2*n) {
+                       // System.out.println("#");
+                        j++;
+                    }
+                    ans = Math.max(ans,j-i);
+                }else{
+                    j++;
+                }
+            }
+            out.append(ans).append('\n');
+            
+        }
+
+        System.out.print(out);
+    }
+}
