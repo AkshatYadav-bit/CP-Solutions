d4db492d6df90dccbfdfa5c8b607907ee394c18b
Akshat Yadav <akshatyadavcv@gmail.com>
2026-09-08 11:51:31 +0530

    Add Count-Commas-in-Range (math)

--- leetcode/math/3870-Count-Commas-in-Range.java
+++ leetcode/math/3870-Count-Commas-in-Range.java
@@ -1,8 +1,6 @@
 class Solution {
     public int countCommas(int n) {
-        int size = (int)Math.floor(Math.log10(n)+1);
         int s = Math.max(0,n - 999);
-        
         return s;
     }
 }
