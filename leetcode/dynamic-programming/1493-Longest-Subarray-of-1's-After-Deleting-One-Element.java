b3fa47c24cafc818790f293a92f3804a2c6c2627
Akshat Yadav <akshatyadavcv@gmail.com>
2026-09-23 15:17:26 +0530

    Add Longest-Subarray-of-1's-After-Deleting-One-Element (dynamic-programming)

--- leetcode/dynamic-programming/1493-Longest-Subarray-of-1's-After-Deleting-One-Element.java
+++ leetcode/dynamic-programming/1493-Longest-Subarray-of-1's-After-Deleting-One-Element.java
@@ -2,20 +2,19 @@ class Solution {
     public int longestSubarray(int[] arr) {
         int max =0;
         int n = arr.length;
-        Map<Integer,Integer> map = new HashMap<>();
+        Map<Integer,Integer> map  = new HashMap<>();
         int i  = 0;
         int j = 0;
         while ( j < n){
             map.put(arr[j],map.getOrDefault(arr[j],0)+1);
-            while(i < n && map.size() > 1 && map.get(0) > 1){
+            while(i < n && map.size() > 0 && map.containsKey(0) && map.get(0) > 1){
                 if(arr[i] == 0) map.put(0,map.get(0)-1);
                 if(arr[i] == 1){
                     map.put(1,map.get(1)-1);
                 }
                 i++;
             }
-            if(map.containsKey(1) && map.containsKey(0)) max = Math.max(max,map.get(1));
-            else if(map.containsKey(1)) max = Math.max(max,map.get(1)-1);
+            max = Math.max(max,j-i);
             j++;
         }
         return max;
